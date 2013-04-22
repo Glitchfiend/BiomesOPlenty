b00f7e71dd3810570f69b03bab528affc4450835
Adubbz <a.dubbz@hotmail.com>
2013-04-22 22:48:57 +1000

    Fixed missing texture error

--- src/minecraft/biomesoplenty/items/ItemBOPFoliage.java
+++ src/minecraft/biomesoplenty/items/ItemBOPFoliage.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemBOPFoliage extends ItemColored
 {
-    private static final String[] foliageTypes = new String[] {"algae", "shortgrass", "mediumgrass", "highgrass", "bush", "sprout", "highgrasstop"};
+    private static final String[] foliageTypes = new String[] {"algae", "shortgrass", "mediumgrass", "item_highgrass", "bush", "sprout", "highgrasstop"};
     @SideOnly(Side.CLIENT)
     private Icon[] textures;
     
