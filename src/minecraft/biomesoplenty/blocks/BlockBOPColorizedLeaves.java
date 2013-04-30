cc2f9f8f87ef515e4e06bf1f1d2254e9ccde8fc5
Amnet <amnet@o2.pl>
2013-04-30 18:08:42 +0200

    Fixed leaves decay.

--- src/minecraft/biomesoplenty/blocks/BlockBOPColorizedLeaves.java
+++ src/minecraft/biomesoplenty/blocks/BlockBOPColorizedLeaves.java
@@ -153,7 +153,7 @@ public class BlockBOPColorizedLeaves extends BlockLeavesBase implements IShearab
 
         int meta = world.getBlockMetadata(x, y, z);
 
-        if ((meta & 8) != 0 && (meta & 4) == 0)
+        if ((meta & 8) != 0/* && (meta & 4) == 0*/)
         {
             byte b0 = 4;
             int i1 = b0 + 1;
