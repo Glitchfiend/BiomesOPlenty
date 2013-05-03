d880bd5f618a797313b573737a156d93cc0964af
Matt Caughey <Catcwe316@comcast.net>
2013-05-03 08:42:16 -0400

    Setting up SourceTree, yet againn

--- src/minecraft/forestry/api/arboriculture/TreeManager.java
+++
@@ -1,7 +0,0 @@
-package forestry.api.arboriculture;
-
-public class TreeManager {
-	public static int treeSpeciesCount = 0;
-	public static ITreeInterface treeInterface;
-	public static ITreeBreedingManager breedingManager;
-}
