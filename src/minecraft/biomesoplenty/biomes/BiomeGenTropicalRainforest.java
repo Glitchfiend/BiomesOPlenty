5146469d47bf8efe6ffa9fa937e339141c3f1fad
Matt Caughey <Catcwe316@comcast.net>
2013-05-28 14:49:14 -0400

    Reverted the Tropical Rainforest/Rainforest switch

--- src/minecraft/biomesoplenty/biomes/BiomeGenTropicalRainforest.java
+++ src/minecraft/biomesoplenty/biomes/BiomeGenTropicalRainforest.java
@@ -1,37 +1,45 @@
 package biomesoplenty.biomes;
 
+import java.awt.Color;
 import java.util.Random;
 
+import biomesoplenty.configuration.BOPConfiguration;
 import biomesoplenty.mobs.EntityJungleSpider;
-import biomesoplenty.worldgen.WorldGenRainforestTree1;
+import biomesoplenty.worldgen.WorldGenRainforest1;
+import biomesoplenty.worldgen.WorldGenRainforest2;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.SpawnListEntry;
-import net.minecraft.world.gen.feature.WorldGenTallGrass;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class BiomeGenTropicalRainforest extends BiomeGenBase
 {
     private BiomeDecoratorBOP customBiomeDecorator;
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
 	public BiomeGenTropicalRainforest(int par1)
     {
         super(par1);
+        this.spawnableMonsterList.add(new SpawnListEntry(EntityOcelot.class, 2, 1, 1));
         this.theBiomeDecorator = new BiomeDecoratorBOP(this);
         this.customBiomeDecorator = (BiomeDecoratorBOP)theBiomeDecorator;
-        this.customBiomeDecorator.treesPerChunk = 14;
-        this.customBiomeDecorator.grassPerChunk = 25;
-		this.customBiomeDecorator.pinkFlowersPerChunk = 2;
-        this.customBiomeDecorator.flowersPerChunk = 25;
-		this.customBiomeDecorator.rosesPerChunk = 10;
-		this.customBiomeDecorator.mushroomsPerChunk = 25;
-		this.customBiomeDecorator.orangeFlowersPerChunk = 6;
+        this.customBiomeDecorator.treesPerChunk = 12;
+        this.customBiomeDecorator.grassPerChunk = 7;
+		this.customBiomeDecorator.highGrassPerChunk = 4;
+        this.customBiomeDecorator.reedsPerChunk = 10;
+        this.customBiomeDecorator.waterlilyPerChunk = 2;
+        this.customBiomeDecorator.orangeFlowersPerChunk = 10;
 		this.customBiomeDecorator.generatePumpkins = false;
-        this.spawnableMonsterList.add(new SpawnListEntry(EntityOcelot.class, 2, 1, 1));
+		this.customBiomeDecorator.generateMelons = true;
+		this.customBiomeDecorator.sproutsPerChunk = 2;
+		this.customBiomeDecorator.quicksandPerChunk = 3;
+		this.customBiomeDecorator.poisonIvyPerChunk = 4;
+		this.customBiomeDecorator.lilyflowersPerChunk = 2;
 		this.spawnableMonsterList.add(new SpawnListEntry(EntityJungleSpider.class, 12, 6, 6));
+        this.waterColorMultiplier = 6160128;
     }
 
     /**
@@ -39,23 +47,34 @@ public class BiomeGenTropicalRainforest extends BiomeGenBase
      */
     public WorldGenerator getRandomWorldGenForTrees(Random par1Random)
     {
-        return (WorldGenerator)(par1Random.nextInt(15) == 0 ? this.worldGeneratorForest : (par1Random.nextInt(5) == 0 ? this.worldGeneratorBigTree : new WorldGenRainforestTree1(false)));
-    }
-
-    /**
-     * Gets a WorldGen appropriate for this biome.
-     */
-    public WorldGenerator getRandomWorldGenForGrass(Random par1Random)
-    {
-        return par1Random.nextInt(4) == 0 ? new WorldGenTallGrass(Block.tallGrass.blockID, 2) : new WorldGenTallGrass(Block.tallGrass.blockID, 1);
+        return (WorldGenerator)(par1Random.nextInt(5) == 0 ? new WorldGenRainforest2() : new WorldGenRainforest1(false));
     }
 	
+    public void decorate(World par1World, Random par2Random, int par3, int par4)
+    {
+        super.decorate(par1World, par2Random, par3, par4);
+        int var5 = 3 + par2Random.nextInt(6);
+
+        for (int var6 = 0; var6 < var5; ++var6)
+        {
+            int var7 = par3 + par2Random.nextInt(16);
+            int var8 = par2Random.nextInt(28) + 4;
+            int var9 = par4 + par2Random.nextInt(16);
+            int var10 = par1World.getBlockId(var7, var8, var9);
+
+            if (var10 == Block.stone.blockID)
+            {
+                par1World.setBlock(var7, var8, var9, Block.oreEmerald.blockID, 0, 2);
+            }
+        }
+    }
+
     /**
      * Provides the basic grass color based on the biome temperature and rainfall
      */
     public int getBiomeGrassColor()
     {
-        return 1759340;
+        return 11002176;
     }
 
     /**
@@ -63,6 +82,33 @@ public class BiomeGenTropicalRainforest extends BiomeGenBase
      */
     public int getBiomeFoliageColor()
     {
-        return 1368687;
+        return 8970560;
+    }
+	
+    /**
+     * takes temperature, returns color
+     */
+    public int getSkyColorByTemp(float par1)
+    {
+		if (BOPConfiguration.skyColors = true)
+		{
+        return 12971089;
+		}	
+		else
+		{
+			par1 /= 3.0F;
+
+			if (par1 < -1.0F)
+			{
+				par1 = -1.0F;
+			}
+
+			if (par1 > 1.0F)
+			{
+				par1 = 1.0F;
+			}
+
+			return Color.getHSBColor(0.62222224F - par1 * 0.05F, 0.5F + par1 * 0.1F, 1.0F).getRGB();
+		}
     }
 }
