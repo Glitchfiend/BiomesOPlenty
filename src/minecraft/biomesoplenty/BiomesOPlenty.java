eed737494f8ea4806da9c94c96a09475fef7323a
Adubbz <a.dubbz@hotmail.com>
2013-06-04 17:44:47 +1000

    Upped version number to 0.5.5

--- src/minecraft/biomesoplenty/BiomesOPlenty.java
+++ src/minecraft/biomesoplenty/BiomesOPlenty.java
@@ -44,7 +44,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid="BiomesOPlenty", name="Biomes O' Plenty", version="0.5.4", dependencies="after:Natura")
+@Mod(modid="BiomesOPlenty", name="Biomes O' Plenty", version="0.5.5", dependencies="after:Natura")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class BiomesOPlenty
 {
