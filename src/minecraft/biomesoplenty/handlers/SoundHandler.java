eb4e3fdb14f5797dc4ddfa9ebc8d21d67c9f2f18
Adubbz <a.dubbz@hotmail.com>
2013-07-04 15:22:32 +1000

    Looks like we have to do sounds this way permanently

--- src/minecraft/biomesoplenty/handlers/SoundHandler.java
+++ src/minecraft/biomesoplenty/handlers/SoundHandler.java
@@ -3,7 +3,6 @@ package biomesoplenty.handlers;
 import java.util.logging.Level;
 
 import net.minecraft.client.resources.ResourceLocation;
-import net.minecraftforge.client.event.sound.PlayStreamingEvent;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -12,7 +11,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class SoundHandler 
 {
-	static ResourceLocation[] recordSoundFiles = { new ResourceLocation(/*"biomesoplenty", "records/" + */"bopdisc.ogg"), new ResourceLocation(/*"biomesoplenty", "records/" + */"bopdiscmud.ogg") };
+	static ResourceLocation[] recordSoundFiles = { new ResourceLocation("bopdisc.ogg"), new ResourceLocation("bopdiscmud.ogg") };
 	static String[] soundFiles = { };
 	
 	@SideOnly(Side.CLIENT)
@@ -44,20 +43,5 @@ public class SoundHandler
             	FMLCommonHandler.instance().getFMLLogger().log(Level.WARNING, "[BiomesOPlenty] Failed loading sound file: " + recordSoundFile);
             }
         }
-	}
-	
-	@SideOnly(Side.CLIENT)
-	@ForgeSubscribe
-	public void onPlayStreaming(PlayStreamingEvent event) 
-	{
-		if (event.name == "bopdisc")
-		{
-			//FMLClientHandler.instance().getClient().sndManager.playStreaming("records.biomesoplenty.bopdisc", (float) event.x + 0.5F, (float) event.y + 0.5F, (float) event.z + 0.5F);
-            //System.out.println(event.manager.soundPoolStreaming.getRandomSound().func_110457_b());
-		}
-		else if (event.name == "bopdiscmud")
-		{
-			//FMLClientHandler.instance().getClient().sndManager.playStreaming("records.bopdiscmud", (float) event.x + 0.5F, (float) event.y + 0.5F, (float) event.z + 0.5F);
-		}
 	}
 }
