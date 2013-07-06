fe39bd01891e1825ac77ae0f8b36528ac15b0adc
Adubbz <a.dubbz@hotmail.com>
2013-07-06 15:18:35 +1000

    And fixed the record path thanks to Lex, turns out i'm a fucking retard

--- src/minecraft/biomesoplenty/handlers/SoundHandler.java
+++ src/minecraft/biomesoplenty/handlers/SoundHandler.java
@@ -2,16 +2,17 @@ package biomesoplenty.handlers;
 
 import java.util.logging.Level;
 
-import net.minecraft.client.resources.ResourceLocation;
+import net.minecraftforge.client.event.sound.PlayStreamingEvent;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class SoundHandler 
 {
-	static ResourceLocation[] recordSoundFiles = { new ResourceLocation("bopdisc.ogg"), new ResourceLocation("bopdiscmud.ogg") };
+	static String[] recordSoundFiles = { "biomesoplenty:bopdisc.ogg", "biomesoplenty:bopdiscmud.ogg" };
 	static String[] soundFiles = { };
 	
 	@SideOnly(Side.CLIENT)
@@ -31,11 +32,11 @@ public class SoundHandler
 			}
 		}
 		
-		for (ResourceLocation recordSoundFile : recordSoundFiles) 
+		for (String recordSoundFile : recordSoundFiles) 
         {
             try 
             {
-                event.manager.soundPoolStreaming.addSound(recordSoundFile.func_110623_a());
+                event.manager.soundPoolStreaming.addSound(recordSoundFile);
             }
 
             catch (Exception e) 
@@ -43,5 +44,19 @@ public class SoundHandler
             	FMLCommonHandler.instance().getFMLLogger().log(Level.WARNING, "[BiomesOPlenty] Failed loading sound file: " + recordSoundFile);
             }
         }
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@ForgeSubscribe
+	public void onPlayStreaming(PlayStreamingEvent event) 
+	{
+		if (event.name == "bopdisc")
+		{
+			FMLClientHandler.instance().getClient().sndManager.playStreaming("biomesoplenty:bopdisc", (float) event.x + 0.5F, (float) event.y + 0.5F, (float) event.z + 0.5F);
+		}
+		else if (event.name == "bopdiscmud")
+		{
+			FMLClientHandler.instance().getClient().sndManager.playStreaming("biomesoplenty:bopdiscmud", (float) event.x + 0.5F, (float) event.y + 0.5F, (float) event.z + 0.5F);
+		}
 	}
 }
