ce6df24e21230c6d3bfa3be05d3795a2918a69f3
Adubbz <a.dubbz@hotmail.com>
2013-07-12 07:48:24 +1000

    Added Russian to the list of Localizations

--- src/minecraft/biomesoplenty/helpers/Localizations.java
+++ src/minecraft/biomesoplenty/helpers/Localizations.java
@@ -10,7 +10,7 @@ public class Localizations
 {
 	private static final String localizationLocation = "/assets/biomesoplenty/lang/";
 
-	public static ResourceLocation[] localeFiles = { new ResourceLocation(localizationLocation + "en_US.xml"), new ResourceLocation(localizationLocation + "de_DE.xml"), new ResourceLocation(localizationLocation + "nl_NL.xml") };
+	public static ResourceLocation[] localeFiles = { new ResourceLocation(localizationLocation + "en_US.xml"), new ResourceLocation(localizationLocation + "de_DE.xml"), new ResourceLocation(localizationLocation + "nl_NL.xml"), new ResourceLocation(localizationLocation + "ru_RU.xml") };
 
 	public static boolean isXMLLanguageFile(ResourceLocation fileName) 
 	{
