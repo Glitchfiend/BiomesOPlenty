be0714862165ca241096753b910ac3eaa1cacb47
Adubbz <a.dubbz@hotmail.com>
2013-10-22 17:06:54 +1100

    Updated and Re-added MFR Integration

--- common/biomesoplenty/integration/MFRIntegration.java
+++ common/biomesoplenty/integration/MFRIntegration.java
@@ -10,7 +10,7 @@ import biomesoplenty.integration.minefactoryreloaded.Harvestable;
 import com.google.common.base.Optional;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import powercrystals.minefactoryreloaded.api.FarmingRegistry;
+import powercrystals.minefactoryreloaded.api.FactoryRegistry;
 import powercrystals.minefactoryreloaded.api.HarvestType;
 
 public class MFRIntegration
@@ -24,24 +24,24 @@ public class MFRIntegration
 
     private static void registerRubberTreeBiomes()
     {
-        FarmingRegistry.registerRubberTreeBiome(Biomes.bayou.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.birchForest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.bog.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.borealForest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.deciduousForest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.forestNew.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.grove.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.jungleNew.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.lushSwamp.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.mapleWoods.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.rainforest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.seasonalForest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.shield.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.swamplandNew.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.temperateRainforest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.thicket.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.tropicalRainforest.get().biomeName);
-        FarmingRegistry.registerRubberTreeBiome(Biomes.woodland.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.bayou.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.birchForest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.bog.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.borealForest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.deciduousForest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.forestNew.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.grove.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.jungleNew.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.lushSwamp.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.mapleWoods.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.rainforest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.seasonalForest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.shield.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.swamplandNew.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.temperateRainforest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.thicket.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.tropicalRainforest.get().biomeName);
+        FactoryRegistry.registerRubberTreeBiome(Biomes.woodland.get().biomeName);
     }
     
     @SuppressWarnings({ "rawtypes", "unchecked" })
@@ -55,37 +55,37 @@ public class MFRIntegration
         
         for(Optional<? extends Block> leaves : bopLeaves)
         {
-            FarmingRegistry.registerHarvestable(new Harvestable(leaves.get().blockID, HarvestType.TreeLeaf));
+            FactoryRegistry.registerHarvestable(new Harvestable(leaves.get().blockID, HarvestType.TreeLeaf));
         }
         
         for(Optional<? extends Block> log : bopLogs)
         {
-            FarmingRegistry.registerHarvestable(new Harvestable(log.get().blockID, HarvestType.Tree));
+            FactoryRegistry.registerHarvestable(new Harvestable(log.get().blockID, HarvestType.Tree));
         }
         
         for(Optional<? extends Block> harvestable : bopMiscStandardHarvestables)
         {
-            FarmingRegistry.registerHarvestable(new Harvestable(harvestable.get().blockID, HarvestType.Normal));
+            FactoryRegistry.registerHarvestable(new Harvestable(harvestable.get().blockID, HarvestType.Normal));
         }
         
         for(Optional<? extends Block> sapling : bopSaplings)
         {
-            FarmingRegistry.registerFertilizable(new Fertilizable(sapling.get().blockID));
+            FactoryRegistry.registerFertilizable(new Fertilizable(sapling.get().blockID));
         }
         
         for(Optional<? extends Block> leaves : bopFruitLeaves)
         {
-            FarmingRegistry.registerHarvestable(new Harvestable(leaves.get().blockID, HarvestType.TreeLeaf));
-            FarmingRegistry.registerFruit(new FruitLeaves(leaves.get().blockID));
+            FactoryRegistry.registerHarvestable(new Harvestable(leaves.get().blockID, HarvestType.TreeLeaf));
+            FactoryRegistry.registerFruit(new FruitLeaves(leaves.get().blockID));
         }
     }
 
     private static void registerSludgeDrops()
     {
-        FarmingRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("driedDirt"));
-        FarmingRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("hardSand"));
-        FarmingRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("hardDirt"));
-        FarmingRegistry.registerSludgeDrop(15, new ItemStack(Items.miscItems.get(), 4, 1));
-        FarmingRegistry.registerSludgeDrop(25, new ItemStack(Items.mudball.get(), 4));
+        FactoryRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("driedDirt"));
+        FactoryRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("hardSand"));
+        FactoryRegistry.registerSludgeDrop(15, BlockReferences.getBlockItemStack("hardDirt"));
+        FactoryRegistry.registerSludgeDrop(15, new ItemStack(Items.miscItems.get(), 4, 1));
+        FactoryRegistry.registerSludgeDrop(25, new ItemStack(Items.mudball.get(), 4));
     }
 }
