21df57a0543fa7570414188df601d90f9d77a457
Adubbz <a.dubbz@hotmail.com>
2013-10-30 18:29:02 +1100

    Fixed wasp hurt highlighting

--- common/biomesoplenty/entities/models/ModelWasp.java
+++ common/biomesoplenty/entities/models/ModelWasp.java
@@ -1,7 +1,5 @@
 package biomesoplenty.entities.models;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
@@ -134,9 +132,6 @@ public class ModelWasp extends ModelBase
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
     {
-        GL11.glRotatef(180F, 0.0F, 1.0F, 0.0F);
-        GL11.glTranslatef(0.0F, 0.75F, 0.0F);
-        
         this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
         
         /*Head*/
--- common/biomesoplenty/entities/render/RenderWasp.java
+++ common/biomesoplenty/entities/render/RenderWasp.java
@@ -1,20 +1,30 @@
 package biomesoplenty.entities.render;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
-import biomesoplenty.entities.models.ModelWasp;
 
+import org.lwjgl.opengl.GL11;
+
+import biomesoplenty.entities.models.ModelWasp;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class RenderWasp extends RenderLiving
 {
     public RenderWasp()
     {
         super(new ModelWasp(), 0.25F);
         this.shadowSize = 0.0F;
+    }
+
+    @Override
+    protected void preRenderCallback(EntityLivingBase entity, float partialTickTime)
+    {
+        GL11.glRotatef(180F, 0.0F, 1.0F, 0.0F);
+        GL11.glTranslatef(0.0F, 0.75F, 0.0F);
     }
 
     @Override
