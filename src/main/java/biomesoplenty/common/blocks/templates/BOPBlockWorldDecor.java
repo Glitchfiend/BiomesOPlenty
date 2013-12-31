4a0b95fed296c3ed4ea415509a2b0f6dded73b33
Adubbz <a.dubbz@hotmail.com>
2013-12-31 12:27:13 +1100

    Fixed flower placement

--- src/main/java/biomesoplenty/common/blocks/templates/BOPBlockWorldDecor.java
+++ src/main/java/biomesoplenty/common/blocks/templates/BOPBlockWorldDecor.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -17,6 +18,9 @@ public abstract class BOPBlockWorldDecor extends BlockBush
 	
 	public boolean isValidPosition(World world, int x, int y, int z, int metadata)
 	{
+    	//TODO:	  getBlock()
+    	if (world.func_147439_a(x, y - 1, z) == Blocks.air) return false;
+		
 		//TODO:	canPlaceBlockAt()
 		return func_149742_c(world, x, y, z);
 	}
@@ -34,12 +38,15 @@ public abstract class BOPBlockWorldDecor extends BlockBush
         this.dropIfCantStay(world, x, y, z, new ItemStack(block, 1, world.getBlockMetadata(x, y, z)));
     }
 	
-	@Override
-	//TODO:			canReplace()
+    @Override
+    //TODO:			canReplace()
     public boolean func_149705_a(World world, int x, int y, int z, int side, ItemStack itemStack)
-	{
-		return isValidPosition(world, x, y, z, itemStack.getItemDamage());
-	} 
+    {
+    	//TODO:	  getBlock()
+    	if (world.func_147439_a(x, y - 1, z) == Blocks.air) return false;
+
+    	return isValidPosition(world, x, y, z, itemStack.getItemDamage());
+    } 
 	
     public void dropIfCantStay(World world, int x, int y, int z, ItemStack stack)
     {
