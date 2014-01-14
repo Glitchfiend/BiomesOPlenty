41642969aefa424f84fbafec48532c8b8617bfa4
Adubbz <a.dubbz@hotmail.com>
2014-01-14 23:03:43 +1100

    And now we no longer override GuiMainMenu. Hackery rocks!

--- src/main/java/biomesoplenty/common/eventhandler/gui/MainMenuEventHandler.java
+++ src/main/java/biomesoplenty/common/eventhandler/gui/MainMenuEventHandler.java
@@ -1,20 +1,23 @@
-package biomesoplenty.client.gui;
+package biomesoplenty.common.eventhandler.gui;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-import biomesoplenty.common.helpers.BOPReflectionHelper;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import biomesoplenty.common.helpers.BOPReflectionHelper;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
-public class GuiMainMenuBOP extends GuiMainMenu
+public class MainMenuEventHandler 
 {
 	public static ResourceLocation[] bopTitlePanoramaPaths = new ResourceLocation[] {new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_0.png"), new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_1.png"), new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_2.png"), new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_3.png"), new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_4.png"), new ResourceLocation("biomesoplenty:textures/gui/title/background/panorama_5.png")};
 	
-	public GuiMainMenuBOP()
-	{
-		super();
-		
-		BOPReflectionHelper.setPrivateFinalValue(GuiMainMenu.class, this, bopTitlePanoramaPaths, "titlePanoramaPaths", "field_73978_o");
-	}
+    @SubscribeEvent
+    public void openMainMenu(GuiOpenEvent event)
+    {
+        if (event.gui instanceof GuiMainMenu)
+        {
+        	GuiMainMenu mainMenu = (GuiMainMenu)event.gui;
+        	
+    		BOPReflectionHelper.setPrivateFinalValue(GuiMainMenu.class, mainMenu, bopTitlePanoramaPaths, "titlePanoramaPaths", "field_73978_o");
+        }
+    }
 }
