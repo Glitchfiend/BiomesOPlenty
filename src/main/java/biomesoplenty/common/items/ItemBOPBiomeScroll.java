8c4ece8b592f64ad0694b907ae8bedc3c6f2cae1
Matt Caughey <Catcwe316@comcast.net>
2014-01-25 08:08:37 -0500

    Improved the Biome Radar texture, added the enchantment effect to the Biome Scrolls

--- src/main/java/biomesoplenty/common/items/ItemBOPBiomeScroll.java
+++ src/main/java/biomesoplenty/common/items/ItemBOPBiomeScroll.java
@@ -2,6 +2,7 @@ package biomesoplenty.common.items;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import biomesoplenty.BiomesOPlenty;
 
 public class ItemBOPBiomeScroll extends Item
@@ -16,4 +17,10 @@ public class ItemBOPBiomeScroll extends Item
     {
         itemIcon = iconRegister.registerIcon("biomesoplenty:biomescroll");
     }
+    
+	@Override
+	public boolean hasEffect(ItemStack itemStack, int pass)
+	{
+		return true;
+	}
 }
