bb1c84fb40a828beba89799be2377c3f31f07fe4
ted80-PC <meatkroket@hotmail.com>
2014-02-01 16:12:05 +0100

    Nether biome gen
    Disabled biome deco: ChunkProviderBOPHell line 570
    Add new hell biomes at: BiomeLayerHellBiomes line 23

---
+++ src/main/java/biomesoplenty/common/world/layer/hell/BiomeLayerHellBiomes.java
@@ -0,0 +1,49 @@
+package biomesoplenty.common.world.layer.hell;
+
+import java.util.ArrayList;
+
+import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.layer.IntCache;
+import biomesoplenty.api.BOPBiomeHelper;
+
+public class BiomeLayerHellBiomes extends BiomeLayerHell
+{
+	private int dimension = 0; 
+	
+	private static ArrayList<BiomeGenBase> netherBiomes = new ArrayList<BiomeGenBase>();
+	
+    public BiomeLayerHellBiomes(long par1, BiomeLayerHell par3GenLayer)
+    {
+        super(par1);
+		parent = par3GenLayer;
+
+		//NETHER BIOMES
+		
+		//add biomes to netherBiomes array here
+
+		if (netherBiomes.size() == 0)
+		{
+			netherBiomes.add(BiomeGenBase.hell);
+		}
+	} 
+
+    @Override
+	public int[] getInts(int par1, int par2, int par3, int par4)
+    {
+        int[] var5 = this.parent.getInts(par1, par2, par3, par4);
+        int[] var6 = IntCache.getIntCache(par3 * par4);
+
+        for (int var7 = 0; var7 < par4; ++var7)
+        {
+            for (int var8 = 0; var8 < par3; ++var8)
+            {
+                this.initChunkSeed((long)(var8 + par1), (long)(var7 + par2));
+                int var9 = var5[var8 + var7 * par3];
+				
+				var6[var8 + var7 * par3] = netherBiomes.get(this.nextInt(netherBiomes.size())).biomeID;
+            }
+        }
+        return var6;
+    }
+}
