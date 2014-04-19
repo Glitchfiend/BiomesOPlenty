3c5414a0e1e8babb74791bd2430683de4e44eaf9
Adubbz <a.dubbz@hotmail.com>
2014-04-20 08:37:09 +1000

    Fixed a crash in the default world

--- src/main/java/biomesoplenty/common/eventhandler/world/DefaultWorldEventHandler.java
+++ src/main/java/biomesoplenty/common/eventhandler/world/DefaultWorldEventHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.world.gen.layer.GenLayerRiverMix;
 import net.minecraftforge.event.terraingen.WorldTypeEvent.InitBiomeGens;
 import biomesoplenty.api.BOPBiomeHelper.TemperatureType;
 import biomesoplenty.common.configuration.BOPConfigurationMain;
+import biomesoplenty.common.core.BOPBiomes;
 import biomesoplenty.common.utils.BOPLogger;
 import biomesoplenty.common.world.BOPBiomeManager.BiomeEntry;
 import biomesoplenty.common.world.layer.GenLayerBiomeBOP;
@@ -19,90 +20,93 @@ public class DefaultWorldEventHandler
 	@SubscribeEvent(priority=EventPriority.LOWEST)
 	public void onInitBiomeGens(InitBiomeGens event)
 	{
-		GenLayer[] originalBiomeGens = event.originalBiomeGens;
-
-		GenLayer parent = originalBiomeGens[0];
-		GenLayerBiomeBOP genLayerBiome = null;
-
-		if (parent instanceof GenLayerRiverMix)
+		if (event.worldType == BOPBiomes.worldTypeBOP)
 		{
-			GenLayerRiverMix genLayerRiverMix = (GenLayerRiverMix)parent;
-			GenLayer biomePatternGeneratorChain = ObfuscationReflectionHelper.getPrivateValue(GenLayerRiverMix.class, genLayerRiverMix, "biomePatternGeneratorChain", "field_75910_b");
+			GenLayer[] originalBiomeGens = event.originalBiomeGens;
 
-			if (biomePatternGeneratorChain != null) parent = biomePatternGeneratorChain;
-		}
+			GenLayer parent = originalBiomeGens[0];
+			GenLayerBiomeBOP genLayerBiome = null;
 
-		while (genLayerBiome == null)
-		{
-			if (parent instanceof GenLayerBiomeBOP)
+			if (parent instanceof GenLayerRiverMix)
 			{
-				genLayerBiome = (GenLayerBiomeBOP)parent;
+				GenLayerRiverMix genLayerRiverMix = (GenLayerRiverMix)parent;
+				GenLayer biomePatternGeneratorChain = ObfuscationReflectionHelper.getPrivateValue(GenLayerRiverMix.class, genLayerRiverMix, "biomePatternGeneratorChain", "field_75910_b");
+
+				if (biomePatternGeneratorChain != null) parent = biomePatternGeneratorChain;
 			}
 
-			GenLayer newParent = ObfuscationReflectionHelper.getPrivateValue(GenLayer.class, parent, "parent", "field_75909_a");
-
-			if (newParent == null)
+			while (genLayerBiome == null)
 			{
-				throw new RuntimeException("Failed to find GenLayerBiome in chain");
-			}
-			else
-			{
-				parent = newParent;
-			}
-		}
-
-		if (genLayerBiome != null)
-		{
-			BiomeGenBase[] vanillaDesertBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151623_c");
-			BiomeGenBase[] vanillaWarmBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151621_d");
-			BiomeGenBase[] vanillaCoolBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151622_e");
-			BiomeGenBase[] vanillaIcyBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151620_f");
-			
-			for (BiomeGenBase biome : vanillaDesertBiomes)
-			{
-				if (biome != null)
+				if (parent instanceof GenLayerBiomeBOP)
 				{
-					if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
-					
-					BiomeEntry entry = new BiomeEntry(biome, TemperatureType.HOT, 10);
-					
-					genLayerBiome.desertBiomes.add(entry);
+					genLayerBiome = (GenLayerBiomeBOP)parent;
+				}
+
+				GenLayer newParent = ObfuscationReflectionHelper.getPrivateValue(GenLayer.class, parent, "parent", "field_75909_a");
+
+				if (newParent == null)
+				{
+					throw new RuntimeException("Failed to find GenLayerBiome in chain");
+				}
+				else
+				{
+					parent = newParent;
 				}
 			}
 
-			for (BiomeGenBase biome : vanillaWarmBiomes)
+			if (genLayerBiome != null)
 			{
-				if (biome != null)
-				{
-					if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
-					
-					BiomeEntry entry = new BiomeEntry(biome, TemperatureType.WARM, 10);
+				BiomeGenBase[] vanillaDesertBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151623_c");
+				BiomeGenBase[] vanillaWarmBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151621_d");
+				BiomeGenBase[] vanillaCoolBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151622_e");
+				BiomeGenBase[] vanillaIcyBiomes = (BiomeGenBase[])ObfuscationReflectionHelper.getPrivateValue(GenLayerBiome.class, genLayerBiome, "field_151620_f");
 
-					genLayerBiome.warmBiomes.add(entry);
+				for (BiomeGenBase biome : vanillaDesertBiomes)
+				{
+					if (biome != null)
+					{
+						if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
+
+						BiomeEntry entry = new BiomeEntry(biome, TemperatureType.HOT, 10);
+
+						genLayerBiome.desertBiomes.add(entry);
+					}
 				}
-			}
 
-			for (BiomeGenBase biome : vanillaCoolBiomes)
-			{
-				if (biome != null)
+				for (BiomeGenBase biome : vanillaWarmBiomes)
 				{
-					if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
-					
-					BiomeEntry entry = new BiomeEntry(biome, TemperatureType.COOL, 10);
+					if (biome != null)
+					{
+						if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
 
-					genLayerBiome.coolBiomes.add(entry);
+						BiomeEntry entry = new BiomeEntry(biome, TemperatureType.WARM, 10);
+
+						genLayerBiome.warmBiomes.add(entry);
+					}
 				}
-			}
 
-			for (BiomeGenBase biome : vanillaIcyBiomes)
-			{
-				if (biome != null)
+				for (BiomeGenBase biome : vanillaCoolBiomes)
 				{
-					if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
-					
-					BiomeEntry entry = new BiomeEntry(biome, TemperatureType.ICY, 10);
+					if (biome != null)
+					{
+						if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
 
-					genLayerBiome.icyBiomes.add(entry);
+						BiomeEntry entry = new BiomeEntry(biome, TemperatureType.COOL, 10);
+
+						genLayerBiome.coolBiomes.add(entry);
+					}
+				}
+
+				for (BiomeGenBase biome : vanillaIcyBiomes)
+				{
+					if (biome != null)
+					{
+						if (BOPConfigurationMain.debugMode) BOPLogger.info("Adding biome " + biome.biomeName + " from the default world.");
+
+						BiomeEntry entry = new BiomeEntry(biome, TemperatureType.ICY, 10);
+
+						genLayerBiome.icyBiomes.add(entry);
+					}
 				}
 			}
 		}
