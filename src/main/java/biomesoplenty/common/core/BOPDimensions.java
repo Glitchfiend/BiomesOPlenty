10399034e42b56a97c1d98ab8b091eef20f9a1b5
ted80-PC <meatkroket@hotmail.com>
2014-08-01 00:03:03 +0200

    Registering new end provider

--- src/main/java/biomesoplenty/common/core/BOPDimensions.java
+++ src/main/java/biomesoplenty/common/core/BOPDimensions.java
@@ -1,5 +1,6 @@
 package biomesoplenty.common.core;
 
+import biomesoplenty.common.world.WorldProviderBOPEnd;
 import biomesoplenty.common.world.WorldProviderBOPHell;
 import net.minecraftforge.common.DimensionManager;
 
@@ -14,11 +15,13 @@ public class BOPDimensions
 	
 	private static void unregisterProviders()
 	{
+		DimensionManager.unregisterProviderType(1);
 		DimensionManager.unregisterProviderType(-1);
 	}
 	
 	private static void registerProviders()
 	{
+		DimensionManager.registerProviderType(1, WorldProviderBOPEnd.class, true);
 		DimensionManager.registerProviderType(-1, WorldProviderBOPHell.class, true);
 	}
 	
