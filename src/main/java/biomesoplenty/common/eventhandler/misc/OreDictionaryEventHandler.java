ae6a668a4e7139c537c8bec56a03b43d85b81d11
Adubbz <a.dubbz@hotmail.com>
2014-08-16 10:54:54 +1000

    Fixing code styling

--- src/main/java/biomesoplenty/common/eventhandler/misc/OreDictionaryEventHandler.java
+++ src/main/java/biomesoplenty/common/eventhandler/misc/OreDictionaryEventHandler.java
@@ -4,13 +4,12 @@ import biomesoplenty.api.content.BOPCItems;
 import biomesoplenty.common.blocks.BlockBOPFoliage;
 import net.minecraftforge.oredict.OreDictionary.OreRegisterEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-public class OreDictionaryEventHandler {
-	private static final String TurnipSeedEntry = "seedTurnip";
-		
+public class OreDictionaryEventHandler 
+{
 	@SubscribeEvent
-	public void OreRegister(OreRegisterEvent event)
+	public void onOreRegister(OreRegisterEvent event)
 	{
-		if(event.Name == TurnipSeedEntry && event.Ore.getItem() != BOPCItems.turnipSeeds)
+		if (event.Name == "seedTurnip" && event.Ore.getItem() != BOPCItems.turnipSeeds)
 		{
 			BlockBOPFoliage.turnipSeedStack = event.Ore.copy();
 		}
