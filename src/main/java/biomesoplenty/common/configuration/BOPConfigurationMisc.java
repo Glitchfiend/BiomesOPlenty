b99230a7711af0e8fa5b1ff74fdb12482e4f31ad
RandomV0x3L <randomv0x3l@randomv0x3l.com>
2014-11-20 00:22:56 -0800

    Add config option to disable BoP title panorama

--- src/main/java/biomesoplenty/common/configuration/BOPConfigurationMisc.java
+++ src/main/java/biomesoplenty/common/configuration/BOPConfigurationMisc.java
@@ -13,6 +13,7 @@ public class BOPConfigurationMisc
 	public static boolean skyColors;
 	//public static boolean achievements;
 	public static boolean dungeonLoot;
+	public static boolean titlePanorama;
 
     public static boolean hotSpringsRegeneration;
 	
@@ -31,6 +32,7 @@ public class BOPConfigurationMisc
 			dungeonLoot = config.get("Miscellanious Settings", "Add Custom Dungeon Loot", true).getBoolean(false);
 			hotSpringsRegeneration = config.get("Miscellanious Settings", "Enable Spring Water Regeneration Effect", true).getBoolean(true);
             hotSpringsRegeneration = config.get("Miscellanious Settings", "Behave Normally During Special Events", false).getBoolean(false);
+            		titlePanorama = config.get("Miscellanious Settings", "Enable Biomes O\' Plenty Main Menu Panorama", true).getBoolean(false);
 
 			//Hard-Coded Colors
 			skyColors = config.get("Hard-Coded Colors", "Enable Sky Colors", true).getBoolean(false);
