d99287fb0573e73928b85acb82eeb818e1d7e8e3
Cheeserolls <dan@fullgun.com>
2015-04-28 12:29:30 +0100

    Add client messages to biome finder

--- src/main/java/biomesoplenty/common/item/ItemBiomeFinder.java
+++ src/main/java/biomesoplenty/common/item/ItemBiomeFinder.java
@@ -8,11 +8,14 @@
 
 package biomesoplenty.common.item;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.WorldChunkManager;
@@ -82,23 +85,47 @@ public class ItemBiomeFinder extends Item
         BiomeGenBase biomeToFind = BiomeGenBase.getBiome(nbt.getInteger("biomeIDToFind")); // returns ocean if biomeIDToFind is out of bounds
         
         // both client and server set the 'searching' tag - client to update the rendering, server so it can't be used again too quickly
-        writeNBTSearching(nbt, world);  
+        writeNBTSearching(nbt, world);
         
         // carry out the search on the server, not the client
-        if (world.isRemote) {return stack;}
-        
-        // search for biomeToFind, maximum distance 5000 blocks, 64 blocks between samples
-        BlockPos pos = this.spiralOutwardsLookingForBiome(world, biomeToFind, player.posX, player.posZ, 5000, 64);
-        if (pos == null)
+        if (world.isRemote)
         {
-            writeNBTNotFound(nbt);
+            // client functionality stops here
+            return stack;
         }
         else
-        {
-            writeNBTFound(nbt, pos);
+        {            
+            // server notifies player that search is starting
+            sendChatMessage(player, I18n.format("biome_finder.searching",biomeToFind.biomeName), EnumChatFormatting.DARK_PURPLE);
+            
+            // search for biomeToFind, maximum distance 5000 blocks, 64 blocks between samples
+            BlockPos pos = this.spiralOutwardsLookingForBiome(world, biomeToFind, player.posX, player.posZ, 5000, 64);
+            if (pos == null)
+            {
+                // server notifies player that search was unsuccessful
+                sendChatMessage(player, I18n.format("biome_finder.not_found",biomeToFind.biomeName), EnumChatFormatting.RED);
+                // write not found tag
+                writeNBTNotFound(nbt);
+            }
+            else
+            {
+                // server notifies player that search was successful
+                sendChatMessage(player, I18n.format("biome_finder.found",biomeToFind.biomeName), EnumChatFormatting.GREEN);
+                // write found tag
+                writeNBTFound(nbt, pos);
+            }
+            // It is necessary for the server to return a copy of the stack to make sure that the client successfully replaces it in the inventory
+            return stack.copy();
+            
         }
-        // It is necessary for the server to return a copy of the stack to make sure that the client successfully replaces it in the inventory
-        return stack.copy();
+
+    }
+    
+    public static void sendChatMessage(EntityPlayer player, String msg, EnumChatFormatting color)
+    {
+        ChatComponentText chatComponent = new ChatComponentText(msg);
+        chatComponent.getChatStyle().setColor(color);
+        player.addChatMessage(chatComponent);
     }
     
     public static void writeNBTSearching(NBTTagCompound nbt, World world)
