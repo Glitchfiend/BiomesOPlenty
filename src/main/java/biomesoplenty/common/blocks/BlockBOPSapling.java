eb31520be4420a1b694a032321291b18d86e4853
wwrpg <wwrpg@hotmail.com>
2015-09-02 21:19:55 -0700

    Update BlockBOPSapling.java
    Fixed null itemStack causing crash. Fixes #529 and #519

--- src/main/java/biomesoplenty/common/blocks/BlockBOPSapling.java
+++ src/main/java/biomesoplenty/common/blocks/BlockBOPSapling.java
@@ -72,7 +72,8 @@ public class BlockBOPSapling extends BlockSapling
 	@Override
 	public boolean canReplace(World world, int x, int y, int z, int side, ItemStack itemStack)
 	{
-		return this.canPlaceBlockAt(world, x, y, z) && this.isValidPosition(world, x, y, z, itemStack.getItemDamage());
+		int metadata = itemStack != null ? itemStack.getItemDamage() : 0;
+		return this.canPlaceBlockAt(world, x, y, z) && this.isValidPosition(world, x, y, z, metadata);
 	} 
 	
 	/**
