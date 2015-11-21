9a680e37640a8f9bc3a4f8afef1e9a469b64b123
Adubbz <Adubbz@users.noreply.github.com>
2015-11-21 23:11:30 +1100

    Slightly reduced the fog density in the Ominous Woods

--- src/main/java/biomesoplenty/common/biome/overworld/BiomeGenOminousWoods.java
+++ src/main/java/biomesoplenty/common/biome/overworld/BiomeGenOminousWoods.java
@@ -133,6 +133,6 @@ public class BiomeGenOminousWoods extends BOPOverworldBiome implements IBiomeFog
     @Override
     public float getFogDensity(int x, int y, int z)
     {
-        return 0.1f;
+        return 0.125f;
     }
 }
