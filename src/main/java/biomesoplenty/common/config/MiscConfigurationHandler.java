94c45b4a9a46ea5b312dce5605fdcc0c63a307be
Adubbz <Adubbz@users.noreply.github.com>
2016-01-03 11:16:08 +1100

    Fixed a typo

--- src/main/java/biomesoplenty/common/config/MiscConfigurationHandler.java
+++ src/main/java/biomesoplenty/common/config/MiscConfigurationHandler.java
@@ -46,7 +46,7 @@ public class MiscConfigurationHandler
                 //Check if the player has a trail
                 if (TrailManager.trailsMap.containsKey(PlayerUtil.getClientPlayerUUID()))
                 {
-                    trailVisbilityMode = TrailVisibilityMode.values()[config.getInt("Modify Trail Visibility", "Trail Settings", 0, 0, 1, "0 = All trails visble, 1 = Others can see your trail but you can'")];
+                    trailVisbilityMode = TrailVisibilityMode.values()[config.getInt("Modify Trail Visibility", "Trail Settings", 0, 0, 1, "0 = All trails visble, 1 = Others can see your trail but you can't")];
                 }
             }
         }
