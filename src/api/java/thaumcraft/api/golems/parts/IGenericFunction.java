711f8d94d7f56e5f9d566805788c50f44631e2ff
Adubbz <Adubbz@users.noreply.github.com>
2016-03-18 21:02:27 +1100

    Lets try and get building working

--- src/api/java/thaumcraft/api/golems/parts/IGenericFunction.java
+++
@@ -1,7 +0,0 @@
-package thaumcraft.api.golems.parts;
-
-import thaumcraft.api.golems.IGolemAPI;
-
-public interface IGenericFunction {
-	public void onUpdateTick(IGolemAPI golem);
-}
--- src/api/java/thaumcraft/api/golems/seals/ISealConfigArea.java
+++
@@ -1,8 +0,0 @@
-package thaumcraft.api.golems.seals;
-
-/**
- * This class identifies this seal as using the default area configuration options.
- */
-public interface ISealConfigArea {
-	
-}
