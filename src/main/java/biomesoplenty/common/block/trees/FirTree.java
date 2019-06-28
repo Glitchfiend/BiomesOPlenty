1e1a23b5cd7fd12a91c16b094c8cc7b3c60eab93
Forstride <forstride@gmail.com>
2019-06-28 16:16:40 -0400

    Allowed fir, redwood, and umbran saplings to grow 2x2 trees

--- src/main/java/biomesoplenty/common/block/trees/FirTree.java
+++ src/main/java/biomesoplenty/common/block/trees/FirTree.java
@@ -1,19 +1,29 @@
 package biomesoplenty.common.block.trees;
 
 import biomesoplenty.common.world.gen.feature.BOPBiomeFeatures;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.trees.BigTree;
 import net.minecraft.block.trees.Tree;
 import net.minecraft.world.gen.feature.AbstractTreeFeature;
+import net.minecraft.world.gen.feature.MegaJungleFeature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class FirTree extends Tree
+public class FirTree extends BigTree
 {
    @Nullable
    @Override
    protected AbstractTreeFeature<NoFeatureConfig> getTreeFeature(Random random)
    {
       return BOPBiomeFeatures.FIR_TREE;
+   }
+
+   @Nullable
+   @Override
+   protected AbstractTreeFeature<NoFeatureConfig> getBigTreeFeature(Random random)
+   {
+      return BOPBiomeFeatures.FIR_TREE_LARGE;
    }
 }
--- src/main/java/biomesoplenty/common/block/trees/UmbranTree.java
+++ src/main/java/biomesoplenty/common/block/trees/UmbranTree.java
@@ -1,6 +1,7 @@
 package biomesoplenty.common.block.trees;
 
 import biomesoplenty.common.world.gen.feature.BOPBiomeFeatures;
+import net.minecraft.block.trees.BigTree;
 import net.minecraft.block.trees.Tree;
 import net.minecraft.world.gen.feature.AbstractTreeFeature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
@@ -8,12 +9,19 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class UmbranTree extends Tree
+public class UmbranTree extends BigTree
 {
    @Nullable
    @Override
    protected AbstractTreeFeature<NoFeatureConfig> getTreeFeature(Random random)
    {
       return BOPBiomeFeatures.UMBRAN_TREE;
+   }
+
+   @Nullable
+   @Override
+   protected AbstractTreeFeature<NoFeatureConfig> getBigTreeFeature(Random random)
+   {
+      return BOPBiomeFeatures.TALL_UMBRAN_TREE;
    }
 }
