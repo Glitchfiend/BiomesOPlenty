7e0f2b8894234932e8dcfb78c7477a10296cbb88
Forstride <forstride@gmail.com>
2020-02-22 14:25:04 -0500

    Added OnlyIn client annotation for GUI Event Handler (Closes #1561)

--- src/main/java/biomesoplenty/client/handler/GuiEventHandler.java
+++ src/main/java/biomesoplenty/client/handler/GuiEventHandler.java
@@ -14,6 +14,8 @@ import net.minecraft.client.gui.screen.CreateWorldScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -23,6 +25,7 @@ import net.minecraftforge.fml.common.Mod;
 public class GuiEventHandler
 {
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public static void onPreInitCreateWorld(GuiScreenEvent.InitGuiEvent.Pre event)
     {
         Screen screenGui = event.getGui();
