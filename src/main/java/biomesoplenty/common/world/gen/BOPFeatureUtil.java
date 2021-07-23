3d1a85164faefe73c012ba6a77f178783db8989e
Forstride <forstride@gmail.com>
2021-07-23 19:49:34 -0400

    Fixed tree placement

--- src/main/java/biomesoplenty/common/world/gen/BOPFeatureUtil.java
+++ src/main/java/biomesoplenty/common/world/gen/BOPFeatureUtil.java
@@ -1,12 +1,14 @@
 package biomesoplenty.common.world.gen;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SaplingBlock;
 
 public final class BOPFeatureUtil {
     // Simulates 1.17 tree gen's sapling provider
     public static boolean isSoil(LevelReader level, BlockPos pos) {
-        return Blocks.OAK_SAPLING.defaultBlockState().canSurvive(level, pos);
+        return level.getBlockState(pos).canSustainPlant(level, pos, Direction.UP, (SaplingBlock)Blocks.OAK_SAPLING);
     }
 }
