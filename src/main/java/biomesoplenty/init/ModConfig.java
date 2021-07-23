9435427d01fb07e8cd075737a5a94a1a7960f85c
Adubbz <Adubbz@users.noreply.github.com>
2021-07-24 09:59:01 +1000

    Update file headers

--- src/main/java/biomesoplenty/init/ModConfig.java
+++ src/main/java/biomesoplenty/init/ModConfig.java
@@ -1,9 +1,6 @@
 /*******************************************************************************
- * Copyright 2014-2019, the Biomes O' Plenty Team
- *
- * This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License.
- *
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ * Copyright 2021, the Glitchfiend Team.
+ * All rights reserved.
  ******************************************************************************/
 package biomesoplenty.init;
 
