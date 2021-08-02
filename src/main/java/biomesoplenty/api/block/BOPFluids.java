3f2d6ac023b1c2d235230c8681a16b3abdeeb0b8
Forstride <forstride@gmail.com>
2021-08-02 05:24:39 -0400

    Added Blood liquid and Porous Flesh blocks to the Visceral Heap.  NOTE: The Blood fluid is not 100% finished and will have minor issues.

---
+++ src/main/java/biomesoplenty/api/block/BOPFluids.java
@@ -0,0 +1,9 @@
+package biomesoplenty.api.block;
+
+import net.minecraft.world.level.material.FlowingFluid;
+
+public class BOPFluids
+{
+    public static FlowingFluid FLOWING_BLOOD;
+    public static FlowingFluid BLOOD;
+}
