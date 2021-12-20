5507e257bed4aeba5b8f678303431b4eb6980bb6
Adubbz <Adubbz@users.noreply.github.com>
2021-12-20 17:57:09 +1100

    Added the mediterranean lakes

--- src/main/java/biomesoplenty/api/biome/BOPBiomes.java
+++ src/main/java/biomesoplenty/api/biome/BOPBiomes.java
@@ -30,6 +30,7 @@ public class BOPBiomes
     public static final ResourceKey<Biome> LUSH_DESERT = register("lush_desert");
     public static final ResourceKey<Biome> MARSH = register("marsh");
     public static final ResourceKey<Biome> MEDITERRANEAN_FOREST = register("mediterranean_forest");
+    public static final ResourceKey<Biome> MEDITERRANEAN_LAKES = register("mediterranean_lakes");
     public static final ResourceKey<Biome> MUSKEG = register("muskeg");
     public static final ResourceKey<Biome> OLD_GROWTH_DEAD_FOREST = register("old_growth_dead_forest");
     public static final ResourceKey<Biome> ORCHARD = register("orchard");
