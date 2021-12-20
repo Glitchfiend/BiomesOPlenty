6361cb1fe1005fe2d0fee87529d0a786f1a6abc6
Adubbz <Adubbz@users.noreply.github.com>
2021-12-20 16:07:33 +1100

    Added the bayou

--- src/main/java/biomesoplenty/api/biome/BOPBiomes.java
+++ src/main/java/biomesoplenty/api/biome/BOPBiomes.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.biome.Biome;
 public class BOPBiomes
 {
     public static final ResourceKey<Biome> BAMBOO_BLOSSOM_GROVE = register("bamboo_blossom_grove");
+    public static final ResourceKey<Biome> BAYOU = register("bayou");
     public static final ResourceKey<Biome> CHERRY_BLOSSOM_GROVE = register("cherry_blossom_grove");
     public static final ResourceKey<Biome> COLD_DESERT = register("cold_desert");
     public static final ResourceKey<Biome> CONIFEROUS_FOREST = register("coniferous_forest");
