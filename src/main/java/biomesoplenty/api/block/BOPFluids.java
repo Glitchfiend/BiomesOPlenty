dc03bde9d7b09d28a02725a8413af6332f3819ca
Adubbz <Adubbz@users.noreply.github.com>
2021-12-28 21:18:25 +1100

    Revert master

--- src/main/java/biomesoplenty/api/block/BOPFluids.java
+++
@@ -1,9 +0,0 @@
-package biomesoplenty.api.block;
-
-import net.minecraft.world.level.material.FlowingFluid;
-
-public class BOPFluids
-{
-    public static FlowingFluid FLOWING_BLOOD;
-    public static FlowingFluid BLOOD;
-}
