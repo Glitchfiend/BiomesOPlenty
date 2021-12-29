32f7d1b765b99558d39d84b5942307be75d6577b
Adubbz <Adubbz@users.noreply.github.com>
2021-12-29 18:49:22 +1100

    Removed custom grass color/foliage color resolver as they are unused

--- src/main/java/biomesoplenty/core/ClientProxy.java
+++ src/main/java/biomesoplenty/core/ClientProxy.java
@@ -61,22 +61,6 @@ public class ClientProxy extends CommonProxy
             return blockColors.getColor(state, null, null, tintIndex); },
         	BOPBlocks.SPROUT, BOPBlocks.BUSH, BOPBlocks.CLOVER, BOPBlocks.HUGE_CLOVER_PETAL, BOPBlocks.FLOWERING_OAK_LEAVES,
             BOPBlocks.MAHOGANY_LEAVES, BOPBlocks.PALM_LEAVES, BOPBlocks.WILLOW_LEAVES, BOPBlocks.WILLOW_VINE);
-
-        ColorResolver grassColorResolver = BiomeColors.GRASS_COLOR_RESOLVER;
-        ColorResolver foliageColorResolver = BiomeColors.FOLIAGE_COLOR_RESOLVER;
-        ColorResolver waterColorResolver = BiomeColors.WATER_COLOR_RESOLVER;
-
-        BiomeColors.GRASS_COLOR_RESOLVER = (biome, posX, posZ) ->
-        {
-            ResourceKey<Biome> key = Minecraft.getInstance().level.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).getResourceKey(biome).orElse(null);
-            return grassColorResolver.getColor(biome, posX, posZ);
-        };
-
-        BiomeColors.FOLIAGE_COLOR_RESOLVER = (biome, posX, posZ) ->
-        {
-            ResourceKey<Biome> key = Minecraft.getInstance().level.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).getResourceKey(biome).orElse(null);
-            return foliageColorResolver.getColor(biome, posX, posZ);
-        };
     }
 
     public static int getRainbowBirchColor(BlockAndTintGetter world, BlockPos pos)
