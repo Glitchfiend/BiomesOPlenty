69dee198be6b6bfe13aa10dd529a1c296e217e51
Forstride <forstride@gmail.com>
2023-06-08 23:45:54 -0400

    Added Jacaranda/Snowblossom Leaf particles, fixed Clover block models

--- src/main/java/biomesoplenty/client/handler/ParticleFactoryHandler.java
+++ src/main/java/biomesoplenty/client/handler/ParticleFactoryHandler.java
@@ -7,6 +7,7 @@ package biomesoplenty.client.handler;
 import biomesoplenty.client.particle.DripParticleBOP;
 import biomesoplenty.client.particle.GlowwormParticle;
 import biomesoplenty.client.particle.PusParticle;
+import biomesoplenty.client.particle.LeafParticle;
 import biomesoplenty.core.BiomesOPlenty;
 import biomesoplenty.init.ModParticles;
 import net.minecraftforge.api.distmarker.Dist;
@@ -25,5 +26,15 @@ public class ParticleFactoryHandler
         event.registerSpriteSet(ModParticles.LANDING_BLOOD.get(), DripParticleBOP.BloodLandProvider::new);
         event.registerSpriteSet(ModParticles.PUS.get(), PusParticle.Provider::new);
         event.registerSpriteSet(ModParticles.GLOWWORM.get(), GlowwormParticle.Provider::new);
+        event.registerSpriteSet(ModParticles.JACARANDA_LEAVES.get(), (p_277215_) -> {
+            return (p_277217_, p_277218_, p_277219_, p_277220_, p_277221_, p_277222_, p_277223_, p_277224_) -> {
+                return new LeafParticle(p_277218_, p_277219_, p_277220_, p_277221_, p_277215_);
+            };
+        });
+        event.registerSpriteSet(ModParticles.SNOWBLOSSOM_LEAVES.get(), (p_277215_) -> {
+            return (p_277217_, p_277218_, p_277219_, p_277220_, p_277221_, p_277222_, p_277223_, p_277224_) -> {
+                return new LeafParticle(p_277218_, p_277219_, p_277220_, p_277221_, p_277215_);
+            };
+        });
     }
 }
