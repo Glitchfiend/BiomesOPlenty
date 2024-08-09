8f7d2e06175bcc5775d01f06e2ec092543bf1b5a
Quinn Semele <quinn@compasses.dev>
2024-08-10 00:26:24 +0100

    Fix sign block state validation on 1.21.1 (#2253)

--- common/src/main/java/biomesoplenty/block/entity/SignBlockEntityBOP.java
+++ common/src/main/java/biomesoplenty/block/entity/SignBlockEntityBOP.java
@@ -6,7 +6,6 @@ package biomesoplenty.block.entity;
 
 import biomesoplenty.api.block.BOPBlockEntities;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -14,12 +13,6 @@ public class SignBlockEntityBOP extends SignBlockEntity
 {
     public SignBlockEntityBOP(BlockPos pos, BlockState state)
     {
-        super(pos, state);
-    }
-
-    @Override
-    public BlockEntityType<?> getType()
-    {
-        return BOPBlockEntities.SIGN;
+        super(BOPBlockEntities.SIGN, pos, state);
     }
 }
