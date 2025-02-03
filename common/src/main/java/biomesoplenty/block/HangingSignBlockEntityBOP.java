3f678acec73ae03782660a571ae4b0a1c3b937fe
雨落 <53731501+qyl27@users.noreply.github.com>
2025-02-03 15:49:52 +0800

    Fix hanging sign block validation. (#2328)

--- common/src/main/java/biomesoplenty/block/HangingSignBlockEntityBOP.java
+++ common/src/main/java/biomesoplenty/block/HangingSignBlockEntityBOP.java
@@ -9,17 +9,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
-public class HangingSignBlockEntityBOP extends HangingSignBlockEntity
-{
-    public HangingSignBlockEntityBOP(BlockPos pos, BlockState state)
-    {
+public class HangingSignBlockEntityBOP extends HangingSignBlockEntity {
+    public HangingSignBlockEntityBOP(BlockPos pos, BlockState state) {
         super(pos, state);
     }
 
     @Override
-    public BlockEntityType<?> getType()
-    {
+    public @NotNull BlockEntityType<?> getType() {
         return BOPBlockEntities.HANGING_SIGN;
+    }
+
+    @Override
+    public boolean isValidBlockState(@NotNull BlockState state) {
+        return getType().isValid(state);
     }
 }
